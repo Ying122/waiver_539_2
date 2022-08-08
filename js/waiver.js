ac829f525d910d6ca9a4eb913d8f1ecd7e949e91
grace_g <zg650@nyu.edu>
2022-08-08 18:25:11 +0800

    complete problem 2

--- js/waiver.js
+++ js/waiver.js
@@ -2,10 +2,25 @@
  * problem 1
  */
 
-let figures = document.getElementsByTagName('figure');
+const figures = document.getElementsByTagName('figure');
 
 for (var i = 0; i < figures.length;i ++) {
     figures[i].classList.add('one-third');
 }
+
+/**
+ * problem 2
+ */
+const footer = document.getElementsByTagName('footer')[0];
+const hideFooter = () => {
+    footer.style.display = "none";
+};
+footer.addEventListener('click', hideFooter);
+
+/**
+ * problem 3
+ */
+
+
 
 
